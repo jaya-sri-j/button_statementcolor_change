9526c146f21e7c633aa0c33ccc08dfefcf4166ce
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 10:03:31 +0530

    adding files.

---
+++ button click after change statement color/js/main.js
@@ -0,0 +1,11 @@
+//const btn = document.getElementById('btn');
+//const btn1 = document.getElementById('btn1');
+
+//btn.addEventListener('click', function onClick(event) {
+ //document.body.style.color='green';
+//});
+
+document.getElementById("btn").onclick = function () 
+{
+document.getElementById("output_text").style.color = 'blue';
+ }
